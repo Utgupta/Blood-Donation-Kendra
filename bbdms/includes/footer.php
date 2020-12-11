0f3428d02c739cb213dcc6b867cee5fbf8e4bc8b
Utgupta <Utgupta@users.noreply.github.com>
2020-12-11 11:19:49 +0530

    first commit

---
+++ bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+       <div class="container">
+            <p class="m-0 text-center text-white">Designed By:-<br>UTKARSH GUPTA<br>RAHUL CHHIMPA<br>AKRITI JHALANI</p>
+        </div>
+        <!-- /.container -->
+    </footer>
